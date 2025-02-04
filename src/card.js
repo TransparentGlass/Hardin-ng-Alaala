867ddc7efa3e65a96472aaa4246441e49d1f4733
Glass <toshilikescookies@gmail.com>
2025-02-04 12:39:09 +0800

    feat: create a flip card mechanic fix: fix the event mode to interactive

--- src/card.js
+++ src/card.js
@@ -10,30 +10,34 @@ import { Graphics, Container } from "pixi.js"
 export class Card{
     constructor(){
         this.cardContainer = new Container();
+        this.cards = [];
+        this.flippedCard = [];
     }
 
     generateCards(gridSize){
         const padding = 10;
-    
+        
+        
         for (let i=0; i < gridSize * gridSize; i++){
             const card = new Graphics()
             .rect(0,0,100,150)
             .fill('E5E5E5');
-            card.eventMode = true;
+            card.eventMode = 'static';
             card.cursor = 'pointer';
     
     
             let row = Math.floor(i / gridSize);
             let column = (i % gridSize)
-            console.log(`Index: ${i}, row: ${row}, col: ${column}`)
     
             card.x = column * (card.width + padding);
             card.y = row * (card.height + padding);
-            console.log(`Card x: ${card.x}, card y: ${card.y}`);
+            card.on('pointerdown', () => this.flipCard(card, i));
             
-    
+            this.cards.push({sprite: card, value: i % (gridSize*gridSize /2), flipped: false})
+            
             this.cardContainer.addChild(card);
         }
+        console.log(this.cards);
     }
 
     getCardContainer(){
@@ -41,6 +45,46 @@ export class Card{
     }
     
     //TODO: place individual cards into a list with their states.
+    flipCard(card, index){
+        if (this.cards[index].flipped){
+            console.log(`the card ${index} is already flipped`)
+            return;
+        } else if (this.flippedCard.length == 2){
+            console.log(`the 2 cards has been flipped`)
+            return;
+        }
+        console.log(`this card ${index} is flipped`);
+        this.cards[index].flipped = true;
+
+        card.clear();
+        card.rect(0,0,100,150)
+        card.fill('ba0012')
+        this.flippedCard.push({card, index});
+        console.log(this.flippedCard)
+        
+        if (this.flippedCard.length == 2){
+            setTimeout(() => {
+                console.log('checking...');
+                for (let i = 0; i < this.flippedCard.length; i++){
+                    console.log(`The cards in this list is: ${this.flippedCard[i].index}`)
+
+                    let currentCard = this.flippedCard[i].card ;
+
+                    this.cards[this.flippedCard[i].index].flipped = false;
+                    currentCard.clear()
+                    .rect(0,0,100,150)
+                    .fill('E5E5E5');
+
+    
+                }
+                this.flippedCard = [];
+            }, 1000)
+            
+        }
+    }
+
+    
+
     //TODO: change state(for now color) if card is clicked
 
 }
