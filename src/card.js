e5454700a0e50c59c3cd2bb37b0e4bbd60573ca4
Glass <toshilikescookies@gmail.com>
2025-02-04 13:36:26 +0800

    feat:  create a card matching mechanic

--- src/card.js
+++ src/card.js
@@ -58,6 +58,7 @@ export class Card{
             console.log(`the 2 cards has been flipped`)
             return;
         }
+
         console.log(`this card ${index} is flipped`);
         this.cards[index].flipped = true;
 
@@ -68,22 +69,9 @@ export class Card{
         this.flippedCard.push({card, index});
         
         if (this.flippedCard.length == 2){
-            setTimeout(() => {
-                console.log('checking...');
-                for (let i = 0; i < this.flippedCard.length; i++){
-                    console.log(`The cards in this list is: ${this.flippedCard[i].index}`)
-
-                    let currentCard = this.flippedCard[i].card ;
-
-                    this.cards[this.flippedCard[i].index].flipped = false;
-                    currentCard.clear()
-                    .rect(0,0,100,150)
-                    .fill('E5E5E5'); //back to white
-
-    
-                }
-                this.flippedCard = [];
-            }, 500)
+            console.log('checking...')
+            setTimeout(() =>{this.matchCard()}, 1000)
+            
             
         }
     }
@@ -91,8 +79,41 @@ export class Card{
     
 
     //TODO: make match algorithm for the cards
-    matchCard(){    
+    matchCard(){ 
+        //matched values
+        const card1 = this.cards[this.flippedCard[0].index]
+        const card2 = this.cards[this.flippedCard[1].index]
 
+        if(card1.value == card2.value){
+            console.log(`\n match!`)
+            card1.sprite.eventMode = 'passive';
+            card2.sprite.eventMode = 'passive';
+            this.flippedCard = [];
+            
+
+        }else{
+            console.log('\ntry again')
+            console.log(`Card 1: ${card1.value}, Card 2: ${card2.value}`)
+
+            card1.flipped= false;
+            card2.flipped = false;
+
+            card1.sprite.clear()
+                .rect(0,0,100,150)
+                .fill('E5E5E5'); //back to white
+
+            card2.sprite.clear()
+            .rect(0,0,100,150)
+            .fill('E5E5E5'); //back to white
+            this.flippedCard = [];
+            
+            
+
+
+        }
+
+
+        
     }
 
 }
